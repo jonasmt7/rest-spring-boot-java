ff2712635443be014c1b8cd94fbcb53e703e0027
jonasmt7 <jonasmourat9090@gmail.com>
2026-06-09 00:53:58 -0300

    adicionando projeto

---
+++ src/main/java/br/com/jonasmt7/rest_spring_boot_java/RestSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonasmt7.rest_spring_boot_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBootJavaApplication.class, args);
+	}
+
+}
