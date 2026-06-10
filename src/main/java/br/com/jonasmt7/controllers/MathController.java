fa6f6de3beb9c6cdb19f4edb2239259631fa8e42
jonasmt7 <jonasmourat9090@gmail.com>
2026-06-10 11:34:59 -0300

    adicionando exeptions

--- src/main/java/br/com/jonasmt7/controllers/MathController.java
+++ src/main/java/br/com/jonasmt7/controllers/MathController.java
@@ -1,5 +1,6 @@
 package br.com.jonasmt7.controllers;
 
+import br.com.jonasmt7.exception.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ public class MathController {
     @RequestMapping("/sum/{n1}/{n2}")
     public Double sum(@PathVariable("n1") String n1,@PathVariable("n2") String n2){
 
-        if(!isNumeric(n1) || !isNumeric(n2)) throw new IllegalArgumentException();
+        if(!isNumeric(n1) || !isNumeric(n2)) throw new UnsupportedMathOperationException("Please set a numeric value");
         return convertToDouble(n1) + convertToDouble(n2);
     }
 
