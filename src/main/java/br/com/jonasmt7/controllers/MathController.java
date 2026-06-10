a7bf78617cc3c00cdacc158750b35b7952879955
jonasmt7 <jonasmourat9090@gmail.com>
2026-06-10 11:48:04 -0300

    separando conceitos

--- src/main/java/br/com/jonasmt7/controllers/MathController.java
+++ src/main/java/br/com/jonasmt7/controllers/MathController.java
@@ -1,6 +1,8 @@
 package br.com.jonasmt7.controllers;
 
 import br.com.jonasmt7.exception.UnsupportedMathOperationException;
+import br.com.jonasmt7.math.SimpleMath;
+import br.com.jonasmt7.request.converters.NumberConverter;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,21 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/math")
 public class MathController {
 
+    private SimpleMath math = new SimpleMath();
+
     @RequestMapping("/sum/{n1}/{n2}")
     public Double sum(@PathVariable("n1") String n1,@PathVariable("n2") String n2){
 
-        if(!isNumeric(n1) || !isNumeric(n2)) throw new UnsupportedMathOperationException("Please set a numeric value");
-        return convertToDouble(n1) + convertToDouble(n2);
-    }
-
-    private Double convertToDouble(String strNumber) {
-        return Double.parseDouble(strNumber);
-    }
-
-    private boolean isNumeric(String strNumber) {
-        if(strNumber == null || strNumber.isEmpty()) return false;
-
-        String number = strNumber.replace(",",".");
-        return (number.matches("[-+]?[0-9]*\\.?[0-9]+"));
+        if(!NumberConverter.isNumeric(n1) || !NumberConverter.isNumeric(n2)) throw new UnsupportedMathOperationException("Please set a numeric value");
+        return math.sum(NumberConverter.convertToDouble(n1), NumberConverter.convertToDouble(n2));
     }
 }
---
+++ src/main/java/br/com/jonasmt7/math/SimpleMath.java
@@ -0,0 +1,8 @@
+package br.com.jonasmt7.math;
+
+public class SimpleMath {
+
+    public Double sum(Double n1, Double n2){
+        return n1 + n2;
+    }
+}
